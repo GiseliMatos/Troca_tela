73ed505483a351a7a9d229d21c2b2606d4ac3a84
Giseli Matos <giseli3690@gmail.com>
2026-04-08 20:15:40 -0300

    First commit

---
+++ app/src/main/java/br/edu/utfpr/trocatela/LancamentoActivity.kt
@@ -0,0 +1,41 @@
+package br.edu.utfpr.trocatela
+
+import android.content.Intent
+import android.os.Bundle
+import android.view.View
+import android.widget.TextView
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class LancamentoActivity : AppCompatActivity() {
+
+    private lateinit var etCod : TextView
+    private lateinit var etQtd : TextView
+    private lateinit var etValor : TextView
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_lancamento)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+
+        etCod = findViewById(R.id.etCod)
+        etQtd = findViewById(R.id.etQtd)
+        etValor = findViewById(R.id.etValor)
+    }
+
+    fun btConfirmarOnClick(view: View) {
+        val intent = Intent(this, ConfirmarActivity::class.java)
+        intent.putExtra("cod", etCod.text.toString())
+        intent.putExtra("qtd", etQtd.text.toString())
+        intent.putExtra("valor", etValor.text.toString())
+        startActivity(intent)
+    }
+    fun btListarOnClick(view: View) {}
+}
